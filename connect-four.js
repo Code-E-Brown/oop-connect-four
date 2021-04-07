3c76c461ad27da961c86839d5b4151ffc6069788
cody <cody.brown1993@gmail.com>
2021-04-07 17:01:21 -0400

    New Game and indicating current player

--- connect-four.js
+++ connect-four.js
@@ -1,12 +1,53 @@
 import { Game } from "./game.js";
 
 let game = undefined;
+
+let player1 = document.getElementById("player-1-name");
+let player2 = document.getElementById("player-2-name");
+let formHolder = document.getElementById("form-holder")
+let newGameButton = document.getElementById("new-game")
+let clickTargets = document.getElementById("click-targets")
+
+let updateUI = function () {
+    if (game === undefined) {
+        document.getElementById("board-holder").classList.add("is-invisible")
+    } else if (game !== undefined) {
+        document.getElementById("board-holder").classList.remove("is-invisible")
+        document.getElementById("game-name").innerHTML = `${game.getName()}`
+        if (game.currentPlayer === 1) {
+            clickTargets.classList.add("black")
+            clickTargets.classList.remove("red")
+        } else {
+            clickTargets.classList.remove("black")
+            clickTargets.classList.add("red")
+        }
+    }
+
+}
+
+
 window.addEventListener("DOMContentLoaded", (event) => {
-    let player1 = document.getElementById("player-1-name");
-    let player2 = document.getElementById("player-2-name");
-    let formHolder = document.getElementById("form-holder")
-    formHolder.addEventListener("keyup", (event) =>{
-        event.target.id.includes("player-1")
+
+    formHolder.addEventListener("keyup", (event) => {
+        let name1 = player1.value;
+        let name2 = player2.value;
+        if (name1.length > 0 && name2.length > 0) {
+            document.getElementById("new-game").disabled = false;
+        }
+    })
+    newGameButton.addEventListener("click", event => {
+        game = new Game(player1.value, player2.value)
+        player1.value = ""
+        player2.value = ""
+        document.getElementById("new-game").disabled = true;
+        updateUI();
+    })
+
+    clickTargets.addEventListener("click", event => {
+
+        game.playInColumn();
+        updateUI();
+
     })
 
 })
--- game.js
+++ game.js
@@ -1,10 +1,18 @@
 export class Game {
-    constructor(playerOne, playerTwo){
+    constructor(playerOne, playerTwo) {
         this.name1 = playerOne;
         this.name2 = playerTwo;
+        this.currentPlayer = 1;
     }
     getName() {
         return `${this.name1} vs. ${this.name2}`;
+    }
+    playInColumn() {
+        if (this.currentPlayer === 1) {
+            this.currentPlayer = 2;
+        } else {
+            this.currentPlayer = 1;
+        }
     }
 }
 
